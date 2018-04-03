d33aa098dfc142979ac3e3511234e5921e51fd42
Denis Chencov <denis@rob1nzon.ru>
2018-04-03 17:30:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ $(function() {
   data.value1 = NameInput.value+" "+EmailInput.value+" "+PhoneInput.value;
   order = document.getElementsByClassName('itemRow');
   order_str='';
-for (var i = 0; i < length(order); i++) {
+for (var i = 0; i < order.length; i++) {
    order_str+=order[i].getElementsByClassName('item-name')[0].innerText;
    order_str+=': '
    order_str+=order[i].getElementsByClassName('item-quantity')[0].innerText;
