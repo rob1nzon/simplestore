568ca67dfd84a66628f2ebd36b2f18adfd493b89
Denis Chencov <denis@rob1nzon.ru>
2018-04-03 17:08:26 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ $(function() {
 		url: "https://maker.ifttt.com/trigger/shop/with/key/de4sE6cA9wc2wrC7AcV1wB",
 // 	        email: "you@yours.com",
 		    extra_data: {
-          Value_1: "Socks",
+          Value_1: NameInput,
           Value_2: "12321321"
         }
 	    },
