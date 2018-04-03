27dcefdf3ca12a17ea4538482ae19c7c61bdf3bc
Denis Chencov <denis@rob1nzon.ru>
2018-04-03 13:52:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "Sock store",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
